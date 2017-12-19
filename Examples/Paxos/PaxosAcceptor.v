4522c25bf14dff9ab94f64de1bce0ed058c15928
Anirudh <anirudhx5@gmail.com>
2017-12-19 10:50:59 +0000

    Finally progress on Acceptor round

--- Examples/Paxos/PaxosAcceptor.v
+++ Examples/Paxos/PaxosAcceptor.v
@@ -24,7 +24,6 @@ Variables (proposers: seq nid) (acceptors: seq nid).
 
 Variable a: nid.
 
-Check PaxosProtocol.
 
 Definition paxos := PaxosProtocol proposers acceptors l.
 Notation W := (mkWorld paxos).
@@ -34,8 +33,6 @@ Section AcceptorImplementation.
 (************** Atomic actions **************)
   
 (* Two receive-actions *)
-
-(* This action actually encompasses two receive-transitions *)
 Program Definition tryrecv_prepare_req := act (@tryrecv_action_wrapper W a
       (fun k _ t b => (k == l) && (t == prepare_req)) _).
 Next Obligation.
@@ -50,15 +47,14 @@ Next Obligation.
 Admitted.
 
 (* Two send-actions, e -- id of the current era *)
-Check send_promise_resp_trans.
-Program Definition send_promise_resp e to :=
-  act (@send_action_wrapper W paxos a l (prEq paxos) (send_promise_resp_trans proposers acceptors) _ [:: e] to).
+Program Definition send_promise_resp to :=
+  act (@send_action_wrapper W paxos a l (prEq paxos) (send_promise_resp_trans proposers acceptors) _ [::] to).
 Next Obligation.
   admit.
 Admitted.
 
-Program Definition send_nack_resp e to :=
-  act (@send_action_wrapper W paxos a l (prEq paxos) (send_nack_resp_trans proposers acceptors) _ [:: e] to).
+Program Definition send_nack_resp to :=
+  act (@send_action_wrapper W paxos a l (prEq paxos) (send_nack_resp_trans proposers acceptors) _ [::] to).
 Next Obligation.
   admit.
 Admitted.
@@ -86,7 +82,18 @@ Next Obligation.
   admit.
 Admitted.
 
-(* Step 2: Receive prepare req *)
+Program Definition read_state:
+  {(ecl : (nat * RoleState))}, DHT [a, W]
+  (fun i => loc i = st :-> ecl, 
+   fun r m => loc m = st :-> ecl /\
+              exists (pf : coh (getS m)), r = (getSt a pf).2) :=
+  Do (act (@skip_action_wrapper W a l paxos (prEq paxos) _
+                                (fun s pf => (getSt a pf).2))).
+Next Obligation.
+  admit.
+Admitted.
+
+(* Step 1: Receive prepare req *)
 
 (* Ending condition *)
 Definition r_prepare_req_cond (res : option proposal) := res == None.
@@ -99,6 +106,7 @@ Definition r_prepare_req_inv (e : nat) : cont (option proposal) :=
     then loc i = st :-> (e, APromised psal)
     else loc i = st :-> (e, AInit).
 
+(* Loops until it receives a prepare req *)
 Program Definition receive_prepare_req_loop (e : nat):
   DHT [a, W]
   (fun i => loc i = st :-> (e, AInit),
@@ -110,13 +118,8 @@ Program Definition receive_prepare_req_loop (e : nat):
         (fun _ => Do _ (
            r <-- tryrecv_prepare_req;
            match r with
-           | Some (from, tg, body) =>
-             if (from \in proposers)
-                  (* TODO: need to check round no.? *)
-                  (* && (head 0 body == e) *)
-             then ret _ _ (Some body)
-             else ret _ _ None
-           | None => ret _ _ None
+           | Some (from, tg, body) => ret _ _ body
+           | None => ret _ _ [::]
            end              
         )) None).
 Next Obligation.
@@ -131,11 +134,50 @@ Admitted.
 Next Obligation.
   admit.
 Admitted.
+Next Obligation.
+  admit.
+Admitted.
+
+
+
+(* Finds the promised number from current state *)
+Definition read_promised_number (rs: RoleState) :=
+  match rs with
+  | APromised psal => head 0 psal
+  | _ => 0
+  end.
 
 (* Step 2: Respond promise or nack to the proposer *)
-Program Definition resp_to_req (e : nat) (p': ):
-  {psal : proposal}, DHT [a, W]
-  (fun i => loc i = st :-> (e, APromised psal), (* TODO: state can also be AInit? *)
-   fun (_ : seq nat) m =>
-     loc m = st :-> (e, APromised psal))
-  := Do (send_promise_resp e).
+Program Definition resp_to_prepare_req (e: nat) (prepare_no: nat):
+  {(pinit pfinal: proposal)}, DHT [a, W]
+   (fun i => loc i = st :-> (e, APromised pinit) \/ loc i = st :-> (e, AInit),
+    fun (_ : seq nat) m => loc m = st :-> (e, APromised pinit) \/
+        loc m = st :-> (e, APromised pfinal))
+  := Do (rs <-- read_state;
+         if prepare_no > read_promised_number rs
+         then send_promise_resp prepare_no
+         else send_nack_resp prepare_no).
+Next Obligation.
+  admit.
+Admitted.
+
+
+(* Using resp_to_prepare_req 0 as a 'do nothing' transition for now.
+As 0 will never be > 0 so the acceptor won't send a promise *)
+Program Definition acceptor_round:
+  {(e: nat) (psal: proposal)}, DHT [a, W] 
+  (fun i =>  loc i = st :-> (e, AInit) \/ loc i = st :-> (e, APromised psal),
+   fun (_ : unit) m => loc m = st :-> (e, APromised psal) \/ loc m = st :-> (e, AInit))
+  := 
+    Do _ (e <-- read_round;
+          msg <-- receive_prepare_req_loop e;
+          match msg with
+          | Some (cons p_no p_val) => resp_to_prepare_req e p_no
+          | _  => resp_to_prepare_req e 0
+          end).
+Next Obligation.
+  admit.
+Admitted.
+Next Obligation.
+  admit.
+Admitted.
