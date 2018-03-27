eedca307326c945cf26407d1d297e55bd77d42da
Anirudh <anirudhx5@gmail.com>
2018-03-27 10:53:56 +0100

    Formatted output

--- scripts/paxos.py
+++ scripts/paxos.py
@@ -23,11 +23,13 @@ consensus_value = None
 exit_codes = [p.wait() for p in [a1, a2, a3]]
 print(exit_codes)
 
+# wait for processes to write to files
 time.sleep(2)
+
 for i in range(1, 4):
     line = subprocess.check_output(['tail', '-1', "acceptor{0}.log".format(i)])
     line = line.decode("utf-8")
-    print(line)
+    print(line.rstrip())
 
     pattern = r"got msg in protocol (\d) with tag = (\d), contents = \[(\d); (\d)\]"
     match = re.match(pattern, line)
@@ -37,11 +39,10 @@ for i in range(1, 4):
         consensus_achieved = False
         break
     else:
-        # print("Acceptor %d accepted %s" % (i, match.group(4)))
+        print("Acceptor %d accepted %s" % (i, match.group(4)))
         consensus_value = match.group(4)
 
-print("\n")
-print("=" * 20)
+print("=" * 40)
 
 if consensus_achieved:
     print("\nConsesus achieved on value: %s" % consensus_value)
